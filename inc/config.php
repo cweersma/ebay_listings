d1bd854b6ce3674cb62ff398b7022be659503335
cweersma <cweersma@npcautomotive.com>
2025-03-06 11:40:00 -0600

    Added ruName keys and description

--- inc/config.php
+++ inc/config.php
@@ -9,30 +9,6 @@
 */
 
 /* ------------------------------------------------------------------- */
-
-/* eBay Application Keys (found at https://developer.ebay.com/my/keys)
- *
- * For each environment, provide the following information in $apiauth:
- * (the following three can be found at https://developer.ebay.com/my/keys)
- *
- * "clientId": the App ID
- * "devId": the Dev ID
- * "clientSecret": the Cert ID
-*/
-$apiauth = [
-    "sandbox"=> [
-        "clientId" => "",
-        "devId" => "",
-        "clientSecret" => ""
-    ],
-    "production"=> [
-        "clientId" => "",
-        "devId" => "",
-        "clientSecret" => ""
-    ]
-];
-
-/* ------------------------------------------------------------------- */
 /* Server details */
 
 /* $baseURL must be specified as the URL path corresponding with the directory in which this repository is installed. */
@@ -56,4 +32,39 @@ $db['NIS'] = [
     "username" => "",
     "password" => "",
     "database" => "",
+];
+
+/* ------------------------------------------------------------------- */
+
+/* eBay Application Keys (found at https://developer.ebay.com/my/keys)
+ *
+ * For each environment, provide the following information in $apiauth:
+ * (the following three can be found at https://developer.ebay.com/my/keys)
+ *
+ * "clientId": the App ID
+ * "devId": the Dev ID
+ * "clientSecret": the Cert ID
+ *
+ * "ruName" is the eBay Redirect URL name, which is automatically generated
+ * when you add a Redirect URL under the "Your eBay Sign-in Settings" section on the
+ * User Access Tokens page at https://developer.ebay.com/my/auth?env=sandbox&index=0
+ * (for sandbox) or https://developer.ebay.com/my/auth?env=production&index=0 (for production).
+ *
+ * When creating this Redirect URL, make sure "Your auth accepted URL" points to <<baseURL>>/reauth.php
+ * (where <<baseURL>> is the URL specified for $baseURL above).
+ *
+*/
+$apiauth = [
+    "sandbox"=> [
+        "clientId" => "",
+        "devId" => "",
+        "clientSecret" => "",
+        "ruName" => ""
+    ],
+    "production"=> [
+        "clientId" => "",
+        "devId" => "",
+        "clientSecret" => "",
+        "ruName" => ""
+    ]
 ];
