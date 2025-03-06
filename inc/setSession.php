b7aaba73be256070087e933a5c0502eef7003c9b
cweersma <cweersma@npcautomotive.com>
2025-03-06 11:09:01 -0600

    Fixed relative path

--- inc/setSession.php
+++ inc/setSession.php
@@ -1,7 +1,7 @@
 <?php
 //Set the session variable and use inc/checkAuth.php to validate
 session_start(["env"=>$_GET["env"]]);
-require_once("inc/checkAuth.php"); //This will check the access token and ensure that it's valid for this session
+require_once("checkAuth.php"); //This will check the access token and ensure that it's valid for this session
 
 //We'll get here if checkAuth succeeds, so redirect to main.php
 header('Location: https://nis.npcautomotive.com/ebay_listings/main.php');
