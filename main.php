e7ca3f3ed51f9fe00d2da286b38d78baad776e1f
cweersma <cweersma@npcautomotive.com>
2025-03-06 14:24:06 -0600

    Fixed missing rel attributes

--- main.php
+++ main.php
@@ -6,8 +6,8 @@ require_once("inc/checkAuth.php");
 <html>
     <head>
         <title>NPC eBay Listing Automation (<?php echo $_SESSION['env']; ?>)</title>
-        <link href="style/main.css" type="text/css" />
-        <link href="style/<?php echo $_SESSION['env']; ?>.css" type="text/css" />
+        <link rel="stylesheet" href="style/main.css" type="text/css" />
+        <link rel="stylesheet" href="style/<?php echo $_SESSION['env']; ?>.css" type="text/css" />
     </head>
     <body>
     </body>
