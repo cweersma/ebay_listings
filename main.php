8991f5663965274d6b43aa95b3ed6f8bcf715a77
cweersma <cweersma@npcautomotive.com>
2025-03-06 09:52:40 -0600

    Added initial main page assets

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+//Include this with every page
+require_once("inc/checkAuth.php");
+?>
+<html>
+    <head>
+        <title>NPC eBay Listing Automation (<?php echo $_SESSION['env']; ?>)</title>
+        <link href="/style/main.css" type="text/css" />
+        <link href="/style/<?php echo $_SESSION['env']; ?>.css" type="text/css" />
+    </head>
+    <body>
+    </body>
+</html>
