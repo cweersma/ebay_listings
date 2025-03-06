44a22f89d2c84bcc5400c0c2b1ed5c521393ef67
cweersma <cweersma@npcautomotive.com>
2025-03-06 09:21:11 -0600

    Added redirection

--- inc/setSession.php
+++ inc/setSession.php
@@ -1,4 +1,8 @@
 <?php
 //Set the session variable and use inc/checkAuth.php to validate
 session_start(["env"=>$_GET["env"]]);
-require_once("inc/checkAuth.php");
+require_once("inc/checkAuth.php"); //This will check the access token and ensure that it's valid for this session
+
+//We'll get here if checkAuth succeeds, so redirect to main.php
+header('Location: https://nis.npcautomotive.com/ebay_listings/main.php');
+die();
