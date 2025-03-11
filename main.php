95fdb4561ca5b87e3c30f102ef74e555b1d0c7df
cweersma <cweersma@npcautomotive.com>
2025-03-11 10:55:11 -0500

    Added logout button

--- main.php
+++ main.php
@@ -9,9 +9,20 @@ require_once("inc/checkAuth.php");
         <title>NPC eBay Listing Automation (<?php echo $_SESSION['env']; ?>)</title>
         <link rel="stylesheet" href="style/main.css" type="text/css" />
         <link rel="stylesheet" href="style/<?php echo $_SESSION['env']; ?>.css" type="text/css" />
+        <script>
+            function $(id){
+                return document.getElementById(id);
+            }
+            window.onload = () => {
+                $("logout").addEventListener("click",() => {
+                    window.location = "logout.php";
+                });
+            }
+        </script>
     </head>
     <body>
         <h1>NPC eBay Listing Automation (<?php echo $_SESSION['env']; ?>)</h1>
+        <button id="logout">Log Out</button>
         <a href="apitest.php">eBay API test form</a>
     </body>
 </html>
