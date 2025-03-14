fc5144292a5f187cdd110e565ae2b3991a96266c
cweersma <cweersma@npcautomotive.com>
2025-03-14 15:32:11 -0500

    Reworked empty check for payload and headers

--- inc/makeRequest.php
+++ inc/makeRequest.php
@@ -3,8 +3,13 @@
 session_start();
 require_once("checkAuth.php");
 require_once("apiRequest.php");
-
-$payload = isset($_POST['payload']) ? json_decode($_POST['payload'],true) : null;
-$headers = isset($_POST['headers']) ? json_decode($_POST['headers'], true) : null;
+$payload = null;
+$headers = null;
+if (isset($_POST['payload']){
+    $payload = json_decode($_POST['payload'],true);
+}
+if (isset($_POST['headers'])){
+    $headers = json_decode($_POST['headers'],true);
+}
 
 echo (json_encode(apiRequest($_POST['url'],$_POST['method'] ?? 'GET',$_POST['tokenType'],$payload,$headers)));
