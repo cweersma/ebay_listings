3d83a631e8f26d85ecaac37bb2089bc7e97c5031
cweersma <cweersma@npcautomotive.com>
2025-03-14 15:18:30 -0500

    Added empty checks

--- inc/makeRequest.php
+++ inc/makeRequest.php
@@ -4,7 +4,7 @@ session_start();
 require_once("checkAuth.php");
 require_once("apiRequest.php");
 
-$payload = $_POST['payload'] ? json_decode($_POST['payload'],true) : null;
-$headers = $_POST['headers'] ? json_decode($_POST['headers'], true) : null;
+$payload = !empty($_POST['payload']) ? json_decode($_POST['payload'],true) : null;
+$headers = !empty($_POST['headers']) ? json_decode($_POST['headers'], true) : null;
 
 echo (json_encode(apiRequest($_POST['url'],$_POST['method'] ?? 'GET',$_POST['tokenType'],$payload,$headers)));
