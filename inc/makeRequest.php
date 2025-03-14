aef98c7a714318c8fa1d816a0e12785fdd988349
cweersma <cweersma@npcautomotive.com>
2025-03-14 15:22:58 -0500

    Added empty check in apiRequest call

--- inc/makeRequest.php
+++ inc/makeRequest.php
@@ -7,4 +7,4 @@ require_once("apiRequest.php");
 $payload = !empty($_POST['payload']) ? json_decode($_POST['payload'],true) : null;
 $headers = !empty($_POST['headers']) ? json_decode($_POST['headers'], true) : null;
 
-echo (json_encode(apiRequest($_POST['url'],$_POST['method'] ?? 'GET',$_POST['tokenType'],!empty($payload) ? $payload : null,$headers)));
+echo (json_encode(apiRequest($_POST['url'],$_POST['method'] ?? 'GET',$_POST['tokenType'],!empty($payload) ? $payload : null,!empty($headers) ? $headers : null)));
