6039043fd4244fb35cc518dbd23e337a33b76cca
cweersma <cweersma@npcautomotive.com>
2025-03-20 13:50:50 -0500

    Removed unused dependency

--- inc/apiRequest.php
+++ inc/apiRequest.php
@@ -71,3 +71,53 @@ function apiRequest(string $endpoint, string $method = 'GET', string $tokenType 
     }
     return $response ?? ["status" => "Request completed without response"];
 }
+
+/**
+ * A Server-Sent Events wrapper for apiRequest, to allow status messages to be sent to the browser. This would typically be run within a loop, as part of a batch.
+ *
+ * Behaviors to note:
+ * * This will send headers if headers haven't already been sent. This may result in errors if the calling script attempts to send headers later.
+ * * This function will detect errors returned from the API and output an appropriate event, but it does not treat these as exceptions. If specific error handling
+ * is needed (for logging, etc.) the returned array should be checked for the existence of an "errors" key.
+ *
+ * @param string $endpoint              {@see apiRequest}
+ * @param array $statusMessages         An associative array of status messages to be sent at the appropriate times, containing
+ *                                      one or more of the following keys:
+ *                                          "start" => a message to be sent before the request is made;
+ *                                          "success" => a message to be sent if the request succeeds;
+ *                                          "failure" => a message to be sent if the request fails;
+ *                                          "completion" => a message to be sent when the request completes (regardless of success or failure)
+ *
+ * @param string $method                {@see apiRequest}
+ * @param string $tokenType             {@see apiRequest}
+ * @param array|null $payload           {@see apiRequest}
+ * @param array|null $requestHeaders    {@see apiRequest}
+ * @param int|float|null $progress      (optional) a number indicating the total progress percentage that will have been achieved after the request is made.
+ *                                      This will be sent as the second line of the SSE data for the success, failure, and/or completion statuses.
+ * @return array                        {@see apiRequest}
+ */
+function apiRequestSSE(string $endpoint, array $statusMessages, string $method = 'GET', string $tokenType = 'user', ?array $payload = null, ?array $requestHeaders = null, int|float|null $progress = null) : array {
+    if (!headers_sent()) {
+        //Initialize the SSE response if it hasn't been started already
+        header("Cache-Control: no-cache");
+        header("Content-Type: text/event-stream");
+        ini_set("output_buffering", "0");
+        ini_set("implicit_flush", "1");
+        while (@ob_end_flush());
+        flush();
+    }
+    if (isset($statusMessages["start"])){
+        echo "event: start\n";
+        echo "data: ".$statusMessages["start"]."\n\n";
+    }
+    $response = apiRequest($endpoint,$method,$tokenType,$payload,$requestHeaders);
+    if (isset($response['errors']) && isset($statusMessages['failure'])){
+        echo "event: error\n";
+        echo "data: ".$statusMessages['failure']."\n";
+        echo (isset($progress) ? "data: ".$progress."\n" : "")."\n";
+    }
+    elseif (isset($statusMessages['success'])){
+        echo "event: success\n";
+        echo "data: ".$statusMessages['success']."\n";
+    }
+}
